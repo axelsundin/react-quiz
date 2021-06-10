b3bd0af4b8d6535cf5b0692cf52471703b38501d
shic0 <72131455+shic0@users.noreply.github.com>
2021-06-10 20:48:26 +0200

    added typography and container

--- src/header.tsx
+++ src/header.tsx
@@ -1,5 +1,7 @@
 // header inte material ui
 import React, { Component, CSSProperties } from 'react';
+import { Typography } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
 
 export default function AppHeader() {
     return (
@@ -7,6 +9,13 @@ export default function AppHeader() {
             <h1>Välkommen</h1>
             <h3>Sugen på Quiz?</h3>
         </div>
+   /*  <Container> 
+        <Typography>
+
+        </Typography>  
+    </Container> */
+    
+    
     );
 }
 const headerContainer: CSSProperties = {
