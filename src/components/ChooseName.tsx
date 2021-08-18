9122063ef28693bdc09a5971c01f1b7520a9bb77
axelsundin <axelsundin@hotmail.com>
2021-08-18 15:52:19 +0200

    merged ChooseCategory

--- src/components/ChooseName.tsx
+++ src/components/ChooseName.tsx
@@ -1,21 +1,18 @@
 import { Button, Input } from "@material-ui/core";
-import { PinDropSharp } from "@material-ui/icons";
 import React, { CSSProperties, useState, useContext } from "react";
 
 import { Link } from "react-router-dom";
-import { QuestionState, Difficulty } from "../API";
 import ChooseDifficulty from "./ChooseDifficulty";
 
 import { UserContext } from "../game";
-
+import ErrorBoundary from "./errorBoundary";
 import { type } from "os";
-
+import ChooseCategory from "./ChooseCategory";
 
 export default function ChooseName() {
   const { name, setName, startTrivia } = useContext(UserContext);
   return (
     <div id="gameSettings" style={{ ...centerContent }}>
-     
       <Input
         type="text"
         name="name"
@@ -23,11 +20,9 @@ export default function ChooseName() {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      
-      
-     
-      
+
       <ChooseDifficulty />
+      <ChooseCategory />
 
       {{ name } && (
         <Button onClick={startTrivia}>
@@ -35,12 +30,8 @@ export default function ChooseName() {
         </Button>
       )}
     </div>
-
-    
   );
-  
 }
-
 
 const centerContent: CSSProperties = {
   display: "flex",
